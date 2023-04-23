81220e66fcc12048e9eae257e74ef7d728e11edf
Albin Rönnkvist <albin.ronnkvist@avarda.com>
2023-04-23 11:47:54 +0200

    Add project files.

---
+++ WebApiCaching/Entities/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiCaching.Entities;
+
+public class Article
+{
+    public required string Title { get; set; }
+    public string? Author { get; set; }
+    public required string Content { get; set; }
+    public DateTimeOffset CreatedAtUtc { get; set; }
+    public DateTimeOffset UpdatedAtUtc { get; set; }
+}
